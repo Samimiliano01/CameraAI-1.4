c6b80c4f50f4fe0974518639511484d60a608bf3
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-05-27 15:22:59 +0200

    changes

--- CameraAI-1.4/app.py
+++ CameraAI-1.4/app.py
@@ -1,5 +1,4 @@
 import camera_manager
-import subprocess
 
 print("Hello World!")
 #CameraManager.get_gps()
--- CameraAI-1.4/camera_manager.py
+++ CameraAI-1.4/camera_manager.py
@@ -1,30 +1,33 @@
 import cv2
 import depthai as dai
 
-pipeline = dai.Pipeline()
+def main():
+    print("starting camera")
 
-color_cam = pipeline.createColorCamera()
-color_cam.setBoardSocket(dai.CameraBoardSocket.CAM_A)  # New recommended socket
-color_cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-color_cam.setInterleaved(False)
-color_cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-color_cam.setFps(5)  # Reduce FPS to lower USB load
+    pipeline = dai.Pipeline()
 
-xout = pipeline.createXLinkOut()
-xout.setStreamName("color")
-color_cam.preview.link(xout.input)
-color_cam.setPreviewSize(1920, 1080)
+    color_cam = pipeline.createColorCamera()
+    color_cam.setBoardSocket(dai.CameraBoardSocket.CAM_A)  # New recommended socket
+    color_cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    color_cam.setInterleaved(False)
+    color_cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+    color_cam.setFps(1)  # Reduce FPS to lower USB load
 
-with dai.Device(pipeline) as device:
-    queue = device.getOutputQueue(name="color", maxSize=4, blocking=False)
+    xout = pipeline.createXLinkOut()
+    xout.setStreamName("color")
+    color_cam.preview.link(xout.input)
+    color_cam.setPreviewSize(1280, 720)
 
-    while True:
-        frame = queue.tryGet()
-        if frame is not None:
-            color_frame = frame.getCvFrame()
-            cv2.imshow("Color Camera", color_frame)
+    with dai.Device(pipeline) as device:
+        queue = device.getOutputQueue(name="color", maxSize=4, blocking=False)
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+        while True:
+            frame = queue.tryGet()
+            if frame is not None:
+                color_frame = frame.getCvFrame()
+                cv2.imshow("Color Camera", color_frame)
 
-cv2.destroyAllWindows()
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    cv2.destroyAllWindows()
